2322911a510aa0e5c435c47cc2fa288ce8030f80
linjinjay <32966306+linjinjay@users.noreply.github.com>
2026-06-18 16:11:05 -0700

    Update script.js
    clean up and organize. working on saving to and loading from local storage.

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-const habits = [
-   {
-     name: "Read",
-     completed: false
-   },
-   {
-     name: "Exercise",
-     completed: false
-   },
-   {
-     name: "coding",
-     completed: false
-   },
-   {
-     name: "video",
-     completed: false
-   }
+let habits = [
+
 ];
 
 const habitList = document.getElementById("habit-list");    //the list
 const addButton = document.getElementById("add-button");    //button
 const habitInput = document.getElementById("habit-input");  //text box
 
+//function save to local storage
+function saveHabits() {
+      const stringHabits = JSON.stringify(habits);
+      localStorage.setItem("habitKey", stringHabits);
+}
+
+
+//function load from local storage
+function loadHabits() {
+      const loadHabits = localStorage.getItem(habitKey);
+      const savedHabits = JSON.parse(loadHabits);
+      habits = savedHabits;
+}
+
+//render habit function. Clears the page first. 
 function renderHabits() {
    habitList.innerHTML = "";
 
+//For loop to cycle through the habit array.
    for(let index = 0; index < habits.length; index++) {
       let habit = habits[index];
+
+     //create list variable. Displays the habit list. 
+      const li = document.createElement("li");
+        li.textContent = habit.name;
+
+     //complete button, if completed is true, button will show undo.
       const button = document.createElement("button");
         if (habit.completed) {
            button.textContent = "Undo";
@@ -33,10 +40,27 @@ function renderHabits() {
         else {
           button.textContent = "Complete";
         }
+
+
+
+        button.addEventListener("click", function() {
+          habits[index].completed = 
+          !habits[index].completed;
+          saveHabits();
+          renderHabits();
+        });
+
+      //delete button. 
       const deleteButton = document.createElement("button");
       deleteButton.textContent = "Delete";
-      const li = document.createElement("li");
-        li.textContent = habit.name;
+
+        deleteButton.addEventListener("click", function(){
+          habits.splice(index, 1);
+          saveHabits();
+          renderHabits();
+        });
+
+      //cross off habit if it is completed with a line through. 
         if (habit.completed) {
           li.style.textDecoration = "line-through";
         }
@@ -47,21 +71,12 @@ function renderHabits() {
         li.appendChild(button);
         li.appendChild(deleteButton);
 
-        button.addEventListener("click", function() {
-          habits[index].completed = 
-          !habits[index].completed;
-          renderHabits();
-        });
-
-        deleteButton.addEventListener("click", function(){
-          habits.splice(index, 1);
-          renderHabits();
-        });
    }
 }
 
 renderHabits();
 
+//add habit button
 addButton.addEventListener("click", function() {
 
    const newHabit = habitInput.value;
@@ -71,6 +86,7 @@ addButton.addEventListener("click", function() {
      completed: false
    });
 
+   saveHabits();
    renderHabits();
 
    habitInput.value = "";
